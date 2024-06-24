1f7c6406dec71c6d5940a9110371a73495367d36
Richard Gajda <richardgajda11@gmail.com>
2024-06-23 22:28:40 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import BlogPage from './components/BlogPage';
@@ -10,16 +10,16 @@ import ParticleCanvas from './components/ParticleCanvas';
 function App() {
 	return (
 		<div className="relative bg-background">
-			<Router>
+			<BrowserRouter>
 				<Header />
 				<ParticleCanvas />
 				<Routes>
-					<Route index element={<HomePage />} />
+					<Route path="/" element={<HomePage />} />
 					<Route path="/blog" element={<BlogPage />} />
 					<Route path="/blog/:postId" element={<BlogPost />} />
 				</Routes>
 				<Footer />
-			</Router>
+			</BrowserRouter>
 		</div>
 	);
 }
