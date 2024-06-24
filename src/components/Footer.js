f592c04e7a44fb2c1cad9ad06584440da95340c7
Richard Gajda <richardgajda11@gmail.com>
2024-06-23 21:55:38 -0400

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<footer className="w-full bg-header-black p-4">
+		<footer className="w-full bg-header-black p-4 z-50">
 			<div className="footer-content flex justify-end text-zinc-300 font-mono">
 				<div className="align-middle">
 					<p>© 2024 Richard Gajda. All rights reserved.</p>
